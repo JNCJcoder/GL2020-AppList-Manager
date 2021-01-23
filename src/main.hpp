8b07da6cdc1b99611e2c1e84174290ba8bdc7d85
drawciamage <drawciamage@gmail.com>
2021-01-23 07:50:50 -0300

    GL2020 1.1.5 Changes
    GL2020 Changed limit from 138 to 140.
    INDEXLIMIT is now 140.

--- src/main.hpp
+++ src/main.hpp
@@ -6,10 +6,10 @@
 #include <algorithm>
 #include <cstring>
 
-#define INDEXLIMIT 138
+#define INDEXLIMIT 140
 #define INDEXNAME "AppListIndex.txt"
 #define INDEXSYMBOL '='
-#define VERSION "1.1.2"
+#define VERSION "1.1.3"
 
 void AddToAppList(const unsigned short InitialIndex);
 void DeleteAppList();
